7be0f12d9ec6f48e9a86d9c13a025d35b7dc2fce
William Ghelfi <trumbitta@gmail.com>
2020-11-26 12:06:43 +0100

    feat(nx-plugin-openapi): start adding new options
    re #9

--- packages/nx-plugin-openapi/src/schematics/api-lib/schema.d.ts
+++ packages/nx-plugin-openapi/src/schematics/api-lib/schema.d.ts
@@ -2,8 +2,11 @@ export interface ApiLibSchematicSchema {
   name: string;
   tags?: string;
   directory?: string;
-  sourceSpecLib: string;
-  sourceSpecFileRelativePath: string;
+  isRemoteSpec: boolean;
+  sourceSpecUrl?: string;
+  sourceSpecUrlAuthorizationHeaders?: string;
+  sourceSpecLib?: string;
+  sourceSpecFileRelativePath?: string;
   generator: string;
   additionalProperties?: string;
 }
