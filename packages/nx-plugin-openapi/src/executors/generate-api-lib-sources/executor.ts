92f710015c0c609b10bb820b7474ca2c5cdad1c9
Daniel Breen <daniel.breen@kno2.com>
2021-05-28 11:41:15 -0600

    fix(typo): global-property => global-properties

--- packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/executor.ts
+++ packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/executor.ts
@@ -77,7 +77,7 @@ function generateSources(
     }
 
     if (globalProperties) {
-      args.push(`--global-property=${globalProperties}`);
+      args.push(`--global-properties=${globalProperties}`);
     }
 
     const child = childProcess.spawn('node_modules/.bin/openapi-generator-cli', args);
