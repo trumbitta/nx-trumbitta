50d121bccc17aac5802438dfde4642c841eb6899
William Ghelfi <trumbitta@gmail.com>
2021-05-31 13:08:35 +0200

    style: use named import
    Just an attempt to make github actions behave
    
    re #21

--- packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/executor.ts
+++ packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/executor.ts
@@ -4,7 +4,7 @@ import { logger, ExecutorContext } from '@nrwl/devkit';
 // Third Parties
 import { from, of } from 'rxjs';
 import { map, catchError, switchMap, tap } from 'rxjs/operators';
-import spawn from 'cross-spawn';
+import { spawn } from 'cross-spawn';
 
 // Utils
 import { deleteOutputDir } from '../../utils/delete-output-dir';
