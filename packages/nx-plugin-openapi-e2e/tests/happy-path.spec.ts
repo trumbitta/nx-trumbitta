82d09cd7301d56fe80bdf6ac3a3aae3ed3af93de
William Ghelfi <trumbitta@gmail.com>
2021-07-30 21:30:29 +0000

    test(nx-plugin-openapi-e2e): cut the execution time by ~half
    re #37

--- packages/nx-plugin-openapi-e2e/tests/happy-path.spec.ts
+++ packages/nx-plugin-openapi-e2e/tests/happy-path.spec.ts
@@ -1,99 +1,98 @@
 // Nrwl
-import { ensureNxProject, runNxCommandAsync, runNxCommand, runPackageManagerInstall } from '@nrwl/nx-plugin/testing';
+import { ensureNxProject, runNxCommand, uniq } from '@nrwl/nx-plugin/testing';
 
 describe('Happy-path', () => {
-  // beforeAll(() => {
-  //   ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
-  //   runPackageManagerInstall();
-  // });
+  let apiSpecLibName: string;
+  let apiLibLibName: string;
+
+  beforeAll(() => {
+    ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
+  });
+
+  beforeEach(() => {
+    apiSpecLibName = uniq('api-spec');
+    apiLibLibName = uniq('api-lib');
+  });
 
   it('should work with a local spec', () => {
-    ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
-
-    runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec api-spec --withSample`);
+    runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec ${apiSpecLibName} --withSample`);
 
     runNxCommand(
       [
         'generate',
         '@trumbitta/nx-plugin-openapi:api-lib',
-        'api-lib',
+        apiLibLibName,
         '--generator=typescript-fetch',
-        '--sourceSpecLib=api-spec',
-        `--sourceSpecFileRelativePath=src/api-spec.openapi.yml`,
+        `--sourceSpecLib=${apiSpecLibName}`,
+        `--sourceSpecFileRelativePath=src/${apiSpecLibName}.openapi.yml`,
       ].join(' '),
     );
 
-    const execute = runNxCommand(`run api-lib:generate-sources`);
+    const execute = runNxCommand(`run ${apiLibLibName}:generate-sources`);
 
     // TODO devise proper expectations
     expect(execute).toContain('Done deleting outputDir');
   }, 120000);
 
   it('should work with a remote spec', () => {
-    ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
-
     runNxCommand(
       [
         'generate',
         '@trumbitta/nx-plugin-openapi:api-lib',
-        'api-lib',
+        apiLibLibName,
         '--generator=typescript-fetch',
         '--isRemoteSpec=true',
         '--sourceSpecUrl=https://petstore.swagger.io/v2/swagger.json',
       ].join(' '),
     );
 
-    runNxCommand(`run api-lib:generate-sources`);
+    const execute = runNxCommand(`run ${apiLibLibName}:generate-sources`);
 
     // TODO devise proper expectations
-    expect(true).toBe(true);
+    expect(execute).toContain('Done deleting outputDir');
   }, 120000);
 
   describe('When using the --global-properties option', () => {
     it('should work with just one value', async () => {
-      ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
-
-      runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec api-spec --withSample`);
+      runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec ${apiSpecLibName} --withSample`);
 
       runNxCommand(
         [
           'generate',
           '@trumbitta/nx-plugin-openapi:api-lib',
-          'api-lib-global-properties',
+          apiLibLibName,
           '--generator=typescript-fetch',
-          '--sourceSpecLib=api-spec',
-          `--sourceSpecFileRelativePath=src/api-spec.openapi.yml`,
+          `--sourceSpecLib=${apiSpecLibName}`,
+          `--sourceSpecFileRelativePath=src/${apiSpecLibName}.openapi.yml`,
           `--global-properties=apis`,
         ].join(' '),
       );
 
-      runNxCommand(`run api-lib-global-properties:generate-sources`);
+      const execute = runNxCommand(`run ${apiLibLibName}:generate-sources`);
 
       // TODO devise proper expectations
-      expect(true).toBe(true);
+      expect(execute).toContain('Done deleting outputDir');
     }, 120000);
 
     it('should work with multiple values', async () => {
-      ensureNxProject('@trumbitta/nx-plugin-openapi', 'dist/packages/nx-plugin-openapi');
-
-      runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec api-spec --withSample`);
+      runNxCommand(`generate @trumbitta/nx-plugin-openapi:api-spec ${apiSpecLibName} --withSample`);
 
       runNxCommand(
         [
           'generate',
           '@trumbitta/nx-plugin-openapi:api-lib',
-          'api-lib-global-properties',
+          apiLibLibName,
           '--generator=typescript-fetch',
-          '--sourceSpecLib=api-spec',
-          `--sourceSpecFileRelativePath=src/api-spec.openapi.yml`,
+          `--sourceSpecLib=${apiSpecLibName}`,
+          `--sourceSpecFileRelativePath=src/${apiSpecLibName}.openapi.yml`,
           `--global-properties=apis,supportingFiles=runtime.ts`,
         ].join(' '),
       );
 
-      runNxCommand(`run api-lib-global-properties:generate-sources`);
+      const execute = runNxCommand(`run ${apiLibLibName}:generate-sources`);
 
       // TODO devise proper expectations
-      expect(true).toBe(true);
+      expect(execute).toContain('Done deleting outputDir');
     }, 120000);
   });
 });
