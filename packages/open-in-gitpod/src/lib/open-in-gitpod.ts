6b8d2aa095f9f9d6ee7d751a67c7d14171b06121
William Ghelfi <trumbitta@gmail.com>
2021-10-31 13:18:10 +0000

    feat(open-in-gitpod): replace nodegit with local git
    close #56

--- packages/open-in-gitpod/src/lib/open-in-gitpod.ts
+++ packages/open-in-gitpod/src/lib/open-in-gitpod.ts
@@ -1,18 +1,13 @@
 // Third Parties
 import open from 'open';
-import { Repository } from 'nodegit';
 
 // Utils
 import { gitpodifyUrl, parseOriginUrl } from './utils';
+import { getCurrentBranchName, getOriginUrl } from './git-utils';
 
 export const cli = async () => {
-  const thisRepoPath = process.cwd();
-  const repository = await Repository.open(thisRepoPath);
-
-  const origin = (await repository.getRemote('origin')).url();
-
-  const branchReference = await repository.getCurrentBranch();
-  const branchName = branchReference.shorthand();
+  const origin = getOriginUrl();
+  const branchName = getCurrentBranchName();
 
   const url = `${parseOriginUrl(origin)}/${branchName}`;
   const gitpodifiedUrl = gitpodifyUrl(url);
